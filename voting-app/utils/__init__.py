75e8ed7458aa1dd5e06814f576da2f6c43dd47b0
test <test@git.com>
2016-06-06 22:02:31 -0400

    changed func call to include port and passwrod

--- voting-app/utils/__init__.py
+++ voting-app/utils/__init__.py
@@ -2,13 +2,13 @@ import time
 from redis import Redis, ConnectionError
 
 
-def connect_to_redis(host):
+def connect_to_redis(host, port, password):
     time.sleep(2)
     print "Connecting to redis"
 
     while True:
         try:
-            redis = Redis(host=host, db=0)
+            redis = Redis(host=host, port= port, password = password, db=0)
             redis.ping()
             print "Connected to redis"
             return redis
