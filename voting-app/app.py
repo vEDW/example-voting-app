c03de4e20b377f1a7dda5d268b5187d51f066507
test <test@git.com>
2016-10-19 00:31:23 -0400

    added check for on-prem p-redis service tile backend

--- voting-app/app.py
+++ voting-app/app.py
@@ -17,9 +17,24 @@ option_b = os.getenv('OPTION_B', "Trump")
 hostname = os.getenv('VCAP_APP_HOST') 
 hostnameString = '( IP:' + os.getenv('CF_INSTANCE_IP') + ': Index: ' + os.getenv('CF_INSTANCE_INDEX') + ': Port: ' + os.getenv('CF_INSTANCE_PORT')+ ' )' 
 
-rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
+
+vcap = json.loads(os.environ['VCAP_SERVICES'])
+
+if 'rediscloud' in vcap:
+    rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
+elif 'p-redis' in vcap:
+    rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['p-redis'][0]
+
+#if json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0] is not 'null'
+#    rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
+#if json.loads(os.environ['VCAP_SERVICES'])['p-redis'][0] is not 'null'
+#    rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['p-redis'][0]
+
 credentials = rediscloud_service['credentials']
-redis = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
+
+hostname = credentials['hostname'] if 'hostname' in credentials else credentials['host']
+redis = redis.Redis(host=hostname, port=credentials['port'], password=credentials['password'])
+        
 
 app = Flask(__name__)
 
